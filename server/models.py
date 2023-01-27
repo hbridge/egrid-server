063c8528932d7af78f570902b3f9d99a216bdbf1
Henry Bridge <hbridge@oldfashionedsw.com>
2023-01-27 10:40:40 -0500

    Add radius search to EGridPlant

--- server/models.py
+++ server/models.py
@@ -1,4 +1,6 @@
 from django.contrib.gis.db import models
+from django.contrib.gis.geos import Point
+from django.contrib.gis.measure import Distance
 
 class EGridPlant(models.Model):
   pid = models.IntegerField('Plant ID EGrid 2020', primary_key=True)
@@ -8,3 +10,8 @@ class EGridPlant(models.Model):
   def __str__(self):
     return f'{self.pid}: {self.pname} {self.location}'
   
+  def plants_near(lat: float, lng: float, radius_miles: float):
+    radius_center = Point(lng, lat)
+    return EGridPlant.objects.filter(
+      location__distance_lt=(radius_center, Distance(mi=radius_miles))
+    )
